7a7f5ac9968a9e823c9ec749c2af5dd965fd410f
Yunus-uk <yunuskhansia@yahoo.co.uk>
2021-08-22 23:52:41 +0100

    elif statement

---
+++ elif statement.py
@@ -0,0 +1,11 @@
+pet_type = "fish"
+
+if pet_type == "dog":
+  print("You have a dog.")
+elif pet_type == "cat":
+  print("You have a cat.")
+elif pet_type == "fish":
+  # this is performed
+  print("You have a fish")
+else:
+  print("Not sure!")
